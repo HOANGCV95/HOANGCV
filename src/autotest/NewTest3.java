1af9e5339669485de2455ac636a105427a8551b5
hoangcv <hoangcv@hoangcv.officevnpay.com>
2019-11-14 11:25:13 +0700

    test case 2,3 chrome,firefox

---
+++ src/autotest/NewTest3.java
@@ -0,0 +1,64 @@
+package autotest;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NewTest3 {
+	WebDriver driver;
+
+	  @BeforeTest
+	  public void beforeTest() {
+		  System.setProperty("webdriver.chrome.driver","C:\\Users\\hoangcv\\Desktop\\AUTOTEST\\chromedriver_win32\\chromedriver.exe");
+		  driver=new ChromeDriver();
+	  driver.get("http://live.demoguru99.com/");
+	  
+  }
+  @Test
+  public void TC_1() {
+	  //click vao account
+	  driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
+	  //click vao login
+	  driver.findElement(By.xpath("//button[@id=\"send2\"]")).click();
+	  //Kiem tra hien thi
+	  String errorEmailMassege = driver.findElement(By.xpath("//div[@id='advice-required-entry-email']")).getText(); 
+	  System.out.println("loi mail"+ errorEmailMassege );
+	  String errorPassWordMassege = driver.findElement(By.xpath("//div[@id='advice-required-entry-pass']")).getText(); 
+	  System.out.println("loi pass"+ errorPassWordMassege);
+	  AssertJUnit.assertEquals("This is a required field.",errorEmailMassege); 
+	  AssertJUnit.assertEquals("This is a required field.",errorPassWordMassege); 
+  }
+ 
+  @Test
+  public void TC_2() {
+	  driver.findElement(By.xpath("//input[@id='email' and @name=\"login[username]\"]"))
+	  .sendKeys("1511551@1516125.5454");
+	  driver.findElement(By.xpath("//button[@id=\"send2\"]")).click();
+	  String errorFomatEmail = driver.findElement(By.xpath("//div[@id='advice-validate-email-email']")).getText(); 
+	  AssertJUnit.assertEquals("Please enter a valid email address. For example johndoe@domain.com.",errorFomatEmail);
+  }
+  @Test
+  public void TC_3() {
+	  driver.findElement(By.xpath("//input[@id='email' and @name=\"login[username]\"]"))
+	  .clear();
+	  driver.findElement(By.xpath("//input[@id='email' and @name=\"login[username]\"]"))
+	  .sendKeys("automation@gmail.com");
+	  driver.findElement(By.xpath("//input[ @name=\"login[password]\"]"))
+	  .sendKeys("123");
+	  driver.findElement(By.xpath("//button[@id=\"send2\"]")).click();
+	  String errorFomatPass = driver.findElement(By.xpath("//div[@id='advice-validate-password-pass']")).getText(); 
+	  AssertJUnit.assertEquals("Please enter 6 or more characters without leading or trailing spaces.",errorFomatPass);
+	  	  
+  }
+  
+  
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
